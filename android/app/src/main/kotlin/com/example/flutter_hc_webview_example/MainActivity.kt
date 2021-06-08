efe9c0dabb766c53c0d010366ca2153f1619fcc7
Jorge Sarabia <jorge@sarabiajor.ge>
2021-06-08 10:37:14 -0300

    Show the google page

--- android/app/src/main/kotlin/com/example/flutter_hc_webview_example/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_hc_webview_example/MainActivity.kt
@@ -1,36 +1,58 @@
 package com.example.flutter_hc_webview_example
 
+import android.content.Intent;
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.plugin.common.MethodChannel;
+import androidx.annotation.NonNull
+import io.flutter.embedding.engine.FlutterEngine
 
 class MainActivity: FlutterActivity() {
-  private val CHANNEL = 'ejemplo.hybrid.composition.webview';
-  private val METHOD = 'nativeWebView';
+  private val CHANNEL = "ejemplo.hybrid.composition.webview"
+  private val METHOD = "nativeWebView"
+  private val REQUEST = 1
 
   override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
     super.configureFlutterEngine(flutterEngine)
     MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
       call, result ->
       if (call.method == METHOD) {
+        onLaunchWebView("")
       } else {
         result.notImplemented()
       }
     }
   }
 
-  private void onLaunchWebView(String url){
-    Intent fullScreenIntent = new Intent(this, CountActivity.class);
-    fullScreenIntent.putExtra(CountActivity.EXTRA_COUNTER, count);
-    startActivityForResult(fullScreenIntent, COUNT_REQUEST);
+  private fun onLaunchWebView(url:String){
+    // val startForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult())
+    // { result: ActivityResult ->
+    //     if (result.resultCode == Activity.RESULT_OK) {
+    //         //  you will get result here in result.data
+    //         }
+
+    //     }
+    // }
+
+    // Intent fullScreenIntent = Intent(this, MainActivity.class);
+    // fullScreenIntent.putExtra(WebViewActivity.extraWebView, url);
+    // startActivityForResult(fullScreenIntent, REQUEST);
+    val intent = Intent(this, WebViewActivity::class.java).apply {
+      putExtra("webviewExtra", url)
+    }
+
+    startActivity(intent)
+
+    // startForResult.launch(Intent(activity, CameraCaptureActivity::class.java))
   }
 
-  @Override
-  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-    if (requestCode == COUNT_REQUEST) {
-      if (resultCode == RESULT_OK) {
-        result.success(data.getIntExtra(CountActivity.EXTRA_COUNTER, 0));
-      } else {
-        result.error("ACTIVITY_FAILURE", "Failed while launching activity", null);
-      }
-    }
-  }
+  // @Override
+  // protected fun onActivityResult(int requestCode, int resultCode, Intent data) {
+  //   if (requestCode == REQUEST) {
+  //     if (resultCode == RESULT_OK) {
+  //       result.success(data.getIntExtra(CountActivity.extraWebView, 0));
+  //     } else {
+  //       result.error("ACTIVITY_FAILURE", "Failed while launching activity", null);
+  //     }
+  //   }
+  // }
 }
--- android/app/src/main/kotlin/com/example/flutter_hc_webview_example/WebViewActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_hc_webview_example/WebViewActivity.kt
@@ -1,14 +1,87 @@
 package com.example.flutter_hc_webview_example
 
-  import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Bitmap
+import io.flutter.embedding.android.FlutterActivity
 import android.os.Bundle
+import android.webkit.*
+import android.widget.SearchView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 
-class MainActivity : AppCompatActivity() {
-    public static final String EXTRA_COUNTER = "counter";
-    private String lastUrl;
+
+class WebViewActivity: FlutterActivity() {
+    public val extraWebView = "webviewExtra";
+    private var lastUrl = "";
+    private val baseUrl = "https://www.google.com"
+    private val searchPath = "/search?q="
+
+    lateinit var webView:WebView
+    lateinit var swipeRefresh:SwipeRefreshLayout
+    lateinit var searchView:SearchView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.webview_activity)
+        setContentView(R.layout.activity_main)
+
+        webView = findViewById<WebView>(R.id.webView)
+        swipeRefresh = findViewById(R.id.swipeRefresh)
+        searchView = findViewById(R.id.searchView)
+
+        searchView.setOnQueryTextListener(object:SearchView.OnQueryTextListener{
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                query?.let {
+                    if(URLUtil.isValidUrl(it)){
+                        webView.loadUrl(it)
+                    }else{
+                        webView.loadUrl("$baseUrl$searchPath$it")
+                    }
+                }
+                return false
+            }
+        })
+
+        swipeRefresh.setOnRefreshListener {
+            webView.reload()
+        }
+
+        webView.webChromeClient = object: WebChromeClient() {
+
+        }
+        webView.webViewClient = object :WebViewClient(){
+            override fun shouldOverrideUrlLoading(
+                view: WebView?,
+                request: WebResourceRequest?
+            ): Boolean {
+                return false
+            }
+
+            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                super.onPageStarted(view, url, favicon)
+                searchView.setQuery(url,false)
+                swipeRefresh.isRefreshing = true
+            }
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                swipeRefresh.isRefreshing = false
+            }
+        }
+
+        val settings:WebSettings = webView.settings
+        settings.javaScriptEnabled = true
+
+        webView.loadUrl(baseUrl)
+
+    }
+
+    override fun onBackPressed() {
+        if(webView.canGoBack()){
+            webView.goBack()
+        }else{
+            super.onBackPressed()
+        }
     }
 }
