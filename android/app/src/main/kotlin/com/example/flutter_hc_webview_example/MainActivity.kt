e4578136ac8e6ad05199894abbe663d783adf08f
Jorge R. Sarabia <jorge@sarabiajor.ge>
2021-06-08 14:20:50 -0300

    Feature/add android webview (#1)
    * Refactoring
    
    * Add kotlin file
    
    * Show the google page
    
    * Show with AppCompatActivity()
    
    * Show another page
    
    * Show a page from Flutter
    
    * Refactoring
    
    * Get the last URL
    
    * Refactoring
    
    * Clean

--- android/app/src/main/kotlin/com/example/flutter_hc_webview_example/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_hc_webview_example/MainActivity.kt
@@ -1,6 +1,48 @@
 package com.example.flutter_hc_webview_example
 
+import android.content.Intent;
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.plugin.common.MethodChannel;
+import androidx.annotation.NonNull
+import io.flutter.embedding.engine.FlutterEngine
 
 class MainActivity: FlutterActivity() {
+  private val CHANNEL = "ejemplo.hybrid.composition.webview"
+  private val METHOD = "nativeWebView"
+  private val REQUEST = 1
+
+  lateinit var methodResult:MethodChannel.Result
+
+  override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+    super.configureFlutterEngine(flutterEngine)
+    MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+      call, result ->
+      methodResult = result
+      if (call.method == METHOD) {
+        val url:String = call.arguments()
+        onLaunchWebView(url)
+      } else {
+        result.notImplemented()
+      }
+    }
+  }
+
+  private fun onLaunchWebView(url:String){
+    val intent = Intent(this, WebViewActivity::class.java).apply {
+      putExtra(WebViewActivity().EXTRA_WEBVIEW, url)
+    }
+
+    startActivityForResult(intent, REQUEST);
+  }
+
+  override fun onActivityResult(requestCode:Int, resultCode:Int, data:Intent){
+    if (requestCode == REQUEST) {
+      if (resultCode == RESULT_OK) {
+        var recibido = data.getStringExtra(WebViewActivity().EXTRA_WEBVIEW)
+        methodResult.success(recibido);
+      } else {
+        methodResult.error("ACTIVITY_FAILURE", "Failed while launching activity", null);
+      }
+    }
+  }
 }
---
+++ android/app/src/main/kotlin/com/example/flutter_hc_webview_example/WebViewActivity.kt
@@ -0,0 +1,101 @@
+package com.example.flutter_hc_webview_example
+
+import android.content.Intent;
+import android.graphics.Bitmap
+import io.flutter.embedding.android.FlutterActivity
+import android.os.Bundle
+import android.webkit.*
+import android.widget.SearchView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import androidx.appcompat.app.AppCompatActivity
+
+class WebViewActivity: AppCompatActivity(){
+    public val EXTRA_WEBVIEW = "webviewExtra";
+    private var lastUrl:String = "";
+    private val baseUrl = "https://www.google.com"
+    private val searchPath = "/search?q="
+
+    lateinit var webView:WebView
+    lateinit var swipeRefresh:SwipeRefreshLayout
+    lateinit var searchView:SearchView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Get the Intent that started this activity and extract the string
+        val receivedUrl = intent.getStringExtra(EXTRA_WEBVIEW)
+
+        webView = findViewById<WebView>(R.id.webView)
+        swipeRefresh = findViewById(R.id.swipeRefresh)
+        searchView = findViewById(R.id.searchView)
+
+        searchView.setOnQueryTextListener(object:SearchView.OnQueryTextListener{
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                query?.let {
+                    if(URLUtil.isValidUrl(it)){
+                        webView.loadUrl(it)
+                    }else{
+                        webView.loadUrl("$baseUrl$searchPath$it")
+                    }
+                }
+                return false
+            }
+        })
+
+        swipeRefresh.setOnRefreshListener {
+            webView.reload()
+        }
+
+        webView.webChromeClient = object: WebChromeClient() {}
+
+        webView.webViewClient = object :WebViewClient(){
+            override fun shouldOverrideUrlLoading(
+                view: WebView?,
+                request: WebResourceRequest?
+            ): Boolean {
+                return false
+            }
+
+            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                super.onPageStarted(view, url, favicon)
+                searchView.setQuery(url,false)
+                swipeRefresh.isRefreshing = true
+            }
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                lastUrl = url!!
+                swipeRefresh.isRefreshing = false
+            }
+        }
+
+        val settings:WebSettings = webView.settings
+        settings.javaScriptEnabled = true
+
+        val urlIsNotValid= receivedUrl.isNullOrBlank() || receivedUrl.isNullOrEmpty()
+
+        if(urlIsNotValid || !URLUtil.isValidUrl(receivedUrl)){
+            webView.loadUrl(baseUrl)
+        }else{
+            webView.loadUrl(receivedUrl!!)
+        }
+
+    }
+
+    override fun onBackPressed() {
+        if(webView.canGoBack()){
+            webView.goBack()
+        }else{
+            val intent = Intent().apply {
+                putExtra(EXTRA_WEBVIEW, lastUrl)
+            }
+            setResult(RESULT_OK, intent);
+            finish();
+        }
+    }
+}
